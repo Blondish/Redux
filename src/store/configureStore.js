afccff097e697b75c2762f6c85c0fbd4aa9d8377
Olena Vaskevych <olenaj21@gmail.com>
2020-07-28 00:23:13 +0200

    initial commit to toolkit branch

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { configureStore } from "@reduxjs/toolkit";
 import { devToolsEnhancer } from "redux-devtools-extension";
 import reducer from "./bugs";
 
